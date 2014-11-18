50cefb9cfeeb00f98a6692b66b963cf03a0b10be
elena <elena.15@hotmail.it>
2014-11-18 12:02:24 +0000

    some refactoring on the test file

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,30 +1,33 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzbuzz;
+
+    @Before
+    public void setUp(){
+        fizzbuzz = new FizzBuzz();
+    }
     @Test
     public void isDivisibleByThree(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertTrue(fizzbuzz.isDivisibleByThree(3));
     }
 
     @Test
     public void isNotDivisibleByThree(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertFalse(fizzbuzz.isDivisibleByThree(1));
     }
 
     @Test
     public void isDivisibleByFive(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertTrue(fizzbuzz.isDivisibleByFive(5));
     }
 
     @Test
     public void isNotDivisibleByFive(){
-        FizzBuzz fizzbuzz = new FizzBuzz();
         assertFalse(fizzbuzz.isDivisibleByFive(1));
     }
 }
