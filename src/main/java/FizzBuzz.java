8b3cb72045cfee3404e841861f66ceba98b2ce72
elena <elena.15@hotmail.it>
2014-11-18 12:23:27 +0000

    refactoring done

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,13 +1,17 @@
 public class FizzBuzz {
     public boolean isDivisibleByThree(int number) {
-       return number % 3 == 0;
+        return isDivisibleBy(number, 3);
+    }
+
+    private boolean isDivisibleBy(int number, int divisor) {
+        return number % divisor == 0;
     }
 
     public boolean isDivisibleByFive(int number) {
-        return number % 5 == 0;
+        return isDivisibleBy(number, 5);
     }
 
     public boolean isDivisibleByFifteen(int number) {
-        return number % 15 == 0;
+        return isDivisibleBy(number, 15);
     }
 }
